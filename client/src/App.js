362849fb184beadc6a21ea4b1424078470fd6030
Kunal <singhkunaldeep@gmail.com>
2022-04-07 16:56:40 +0530

    setting up client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { AppBar, Container, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+
+import useStyles from "./styles";
+import memories from "./images/memories.png";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
